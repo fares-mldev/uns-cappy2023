cfc6ec035e204fd1c49b44ce654c43a4159ef087
Federico Ares <fares-mldev@gmail.com>
2023-11-28 12:07:30 -0300

    comentarios np

--- TEORIA/numpy/np.py
+++ TEORIA/numpy/np.py
@@ -6,3 +6,9 @@ print(np.empty(10))
 print(np.linspace(10,42,6))
 print(np.arange(10))
 print(np.random.rand(10,2))
+
+# Guardar y recuperar en csv
+# np.savetxt()
+# np.loadtxt()
+
+# El reshape son solo vistas
