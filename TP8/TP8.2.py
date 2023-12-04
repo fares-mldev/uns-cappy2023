5259f9999b2b65350113d6ec8406fc9c1da227b1
Federico Ares <fares-mldev@gmail.com>
2023-12-04 09:28:45 -0300

    TP8.2

--- TP8/TP8.2.py
+++ TP8/TP8.2.py
@@ -35,7 +35,45 @@ print(f'# a) Palabras que aparecen en dos archivos de texto distintos: {set_1.in
 
 # b) Todas las posiciones en las que un caracter dado aparece en una cadena.
 
+def buscar_posiciones(linea, caracter):
+    posiciones = set()
+    for i in range(len(linea)):
+        if linea[i] == caracter:
+            posiciones.add(i)
+    return posiciones
 
+def buscar_posiciones_archivo(fn,caracter):
+    ret_val=set()
+    with open(fn, 'r') as archivo:
+        for linea in archivo:
+            ret_val=ret_val.union(buscar_posiciones(linea,caracter))
+    return ret_val
+
+caracter='a'
+archivo=archivo_1
+posiciones=buscar_posiciones_archivo(archivo_1,'a')
+
+print(f'b) Todas las posiciones en las que el caracter "{caracter}" aparece en una cadena del archivo {archivo} son: {posiciones}')
 
 # c) Las letras que aparecen en todas las líneas de un archivo de texto.
 
+def buscar_letras(linea):
+    letras_str=''.join(char.lower() for char in linea if char.isalpha())
+    letras_set=set(letras_str)
+    return letras_set
+
+def buscar_letras_archivo(fn):
+    ret_val=set()
+    with open(fn, 'r') as archivo:
+        for linea in archivo:
+            if not ret_val:
+                ret_val=buscar_letras(linea)
+            else:
+                ret_val=ret_val.intersection(buscar_letras(linea))
+    return ret_val
+
+caracter='a'
+archivo=archivo_1
+letras=buscar_letras_archivo(archivo_1)
+
+print(f'c) Las letras que aparecen en todas las líneas del un archivo de texto {archivo} son: {letras}')
